e6f56de5f5c3fa5742ef433314a2a49da5c8d346
Marcel Ochs <marc3lox@googlemail.com>
2021-12-30 23:54:59 +0100

    Updates

---
+++ precache-manifest.b77e93a7062ebfc4a1382070cdc088f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591aac2f9d8a67eabf945f4d00619521",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "1265dc5f85fafa2d048a",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1265dc5f85fafa2d048a",
+    "url": "/home/static/js/main.4fa2e0e6.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "3c1c208164ca0c9f23e5a3894c185bf2",
+    "url": "/home/static/media/portrait.3c1c2081.jpg"
+  },
+  {
+    "revision": "67d76eca2ca29d75bdba0018d22fc9d3",
+    "url": "/home/static/media/resume.67d76eca.pdf"
+  }
+]);
