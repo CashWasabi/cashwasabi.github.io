417c63d2c3ae83700b49a1b0f57aa2cf9195dde8
Marcel Ochs <marc3lox@googlemail.com>
2021-12-31 03:37:33 +0100

    Updates

---
+++ precache-manifest.9f033aac2f6bfca36effe70f90a98b41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea1451500cc25d51b658dc8864e430a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a42a8a8363e837c0f4",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a42a8a8363e837c0f4",
+    "url": "/static/js/main.66615e3e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3c1c208164ca0c9f23e5a3894c185bf2",
+    "url": "/static/media/portrait.3c1c2081.jpg"
+  },
+  {
+    "revision": "67d76eca2ca29d75bdba0018d22fc9d3",
+    "url": "/static/media/resume.67d76eca.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.b77e93a7062ebfc4a1382070cdc088f1.js"
+  "/precache-manifest.9f033aac2f6bfca36effe70f90a98b41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
